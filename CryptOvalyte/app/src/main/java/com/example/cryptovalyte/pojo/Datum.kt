96623f7baaa2afc2721dc874b50b3b58ac257be8
Bahamut1337 <147394405+Bahamut1337@users.noreply.github.com>
2024-01-11 20:42:12 +0200

    Delete CryptOvalyte directory

--- CryptOvalyte/app/src/main/java/com/example/cryptovalyte/pojo/Datum.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cryptovalyte.pojo
-
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-data class Datum(
-    @SerializedName("CoinInfo" ) var CoinInfo : CoinInfo? = CoinInfo()
-)
