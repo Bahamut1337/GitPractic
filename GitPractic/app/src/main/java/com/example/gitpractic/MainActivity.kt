788b6eb59aad618744f2a59cfe4f223748f32d69
Aleksey <bahamutadam@gmail.com>
2024-01-11 19:24:57 +0200

    Git practica 0.1

--- GitPractic/app/src/main/java/com/example/gitpractic/MainActivity.kt
+++ GitPractic/app/src/main/java/com/example/gitpractic/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.gitpractic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gitpractic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
